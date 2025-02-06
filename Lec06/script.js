bf0a292999f091304cb210a4a80e4b1232e8f7fc
ZainabEman <zainabeman976@gmail.com>
2025-02-06 13:38:07 +0500

    manipualting html using script

--- Lec06/script.js
+++ Lec06/script.js
@@ -1,6 +1,14 @@
 // document.getElementById("hello");
 // document.querySelector(.hello)
-let mainEl=document.getElementById("zainab");
+//let mainEl=document.getElementById("zainab");
 // console.log(mainEl.childNodes);
 // console.log(mainEl.firstChild);
-console.log(mainEl.lastChild);
+// console.log(mainEl.lastChild);
+// let mainEl=document.getElementById("hello").innerText="Manipulated";
+// let mainEl=document.getElementsByTagName("zainab").innerHTML="<h1>i am changed</h1>";
+// let mainEl=document.getElementById("ayesha");
+// mainEl.innerText="i am ayesha";
+// mainEl.style.color="purple";
+// mainEl.style.backgroundColor="pink";
+// let myimage=document.getElementById("image");
+// myimage.src="";
