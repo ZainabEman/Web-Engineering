059584c68909a6b4e6250c147194e7de6692e59b
ZainabEman <zainabeman976@gmail.com>
2025-02-06 16:02:43 +0500

    adding html in Document using JS

--- Lec06/script.js
+++ Lec06/script.js
@@ -12,10 +12,24 @@
 // mainEl.style.backgroundColor="pink";
 // let myimage=document.getElementById("image");
 // myimage.src="";
-let myEl=document.createElement("div");
-let myButton=document.createElement("button");
-myButton.innerText="Zainab";
-myEl.appendChild(myButton);
-document.body.appendChild(myEl);
-// myEl.remove;
-myButton.addEventListener("click",()=>console.log("ayesha hoon mainn"))
+ let myEl=document.createElement("div");
+ let myButton=document.createElement("button");
+ myButton.innerText="Zainab";
+ myEl.appendChild(myButton);
+ document.body.appendChild(myEl);
+// // myEl.remove;
+// 
+let count=0;
+function incrementCounter(){
+    return count++;
+}
+myButton.addEventListener("click",()=>{alert('i am clicked')});
+myEl.addEventListener('click', incrementCounter);
+myButton.addEventListener("mouseover", function() {
+    myButton.style.color = "red";
+});
+let element=document.getElementsByTagName("*");
+console.log(element);
+for (let i = 0; i < element.length; ++i) { 
+    console.log(element[i]);
+}
