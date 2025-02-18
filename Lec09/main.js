d2c903f00274a1edfc8f36de0e271305918f7a6a
ZainabEman <zainabeman976@gmail.com>
2025-02-18 13:05:02 +0500

    callback functions

---
+++ Lec09/main.js
@@ -0,0 +1,55 @@
+// console.log("i am zainab");
+// setTimeout(Function(),console.log("i am zainab 02"), 200);
+// console.log("hello");
+
+//callback functions
+// console.log("begin");
+// FetchUser(123, function(user){console.log(user);});
+
+// function FetchUser(userid, callback){
+// setTimeout(function(){
+//     const FetchUser={
+//         id:userid,
+//         name:"zainab",
+//         email:"zainab@gmail.com",
+//         age:21
+//     };callback(FetchUser);
+// }, 200)};
+// console.log("end");
+
+
+// console.log("before");
+// function SendEmail(userEmail, callback){
+//     setTimeout(function(){
+//         const Response={
+//             status:true
+//         };callback(Response);
+//     }, 3000)};
+
+
+//     SendEmail("zainab@gmail.com", callback(response));
+//     console.log("end");
+    
+
+console.log("start");
+const promise= new Promise(function(resolve, reject){
+    setTimeout(
+        reject(new Error("i am filled"));
+        )
+        resolve(1),2000
+    );
+});
+
+console.log(promise);
+
+promise.then(function (result) {
+    console.log(result);
+})
+
+
+
+
+
+
+
+
