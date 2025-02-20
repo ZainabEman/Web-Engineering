968a118ebb2db374197020b4b73187ebbfa70110
ZainabEman <zainabeman976@gmail.com>
2025-02-20 13:05:21 +0500

    api calls

--- Lec10/main.js
+++ Lec10/main.js
@@ -9,6 +9,15 @@
 // Promise.all([p1,p2])
 //     .then(res);
 
-const git=fetch('Https://api.github.com/users/ZainabEman');
-git.then(res=>res.json());
-git.then(profile=>console.log(profile));
+//const git=fetch('Https://api.github.com/users/ZainabEman');
+// git.then(res=>res.json());
+// git.then(profile=>console.log(profile));
+
+// async function getGitData(){
+//     const git=fetch('Https://api.github.com/users/ZainabEman');
+//     const profile=(await git).json();
+//     console.log(profile);
+// };
+
+sessionStorage.setItem('gitURL', 'Https://api.github.com/users/ZainabEman');
+console.log(sessionStorage.getItem('gitURL'));
