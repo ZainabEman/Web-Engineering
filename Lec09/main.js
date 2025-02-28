2e93b9a39ead73a018b2c45a3a161efadb0ca08b
ZainabEman <zainabeman976@gmail.com>
2025-02-28 23:21:10 +0500

    updates

--- Lec09/main.js
+++ Lec09/main.js
@@ -1,59 +1,59 @@
-// console.log("i am zainab");
-// setTimeout(Function(),console.log("i am zainab 02"), 200);
-// console.log("hello");
+// Console Log Examples
+console.log("I am Zainab");
 
-//callback functions
-// console.log("begin");
-// FetchUser(123, function(user){console.log(user);});
+// Using setTimeout Correctly
+setTimeout(() => console.log("I am Zainab 02"), 200);
 
-// function FetchUser(userid, callback){
-// setTimeout(function(){
-//     const FetchUser={
-//         id:userid,
-//         name:"zainab",
-//         email:"zainab@gmail.com",
-//         age:21
-//     };callback(FetchUser);
-// }, 200)};
-// console.log("end");
+console.log("Hello");
 
+// Callback Functions Example
+console.log("Begin");
+FetchUser(123, function(user){
+    console.log(user);
+});
 
-// console.log("before");
-// function SendEmail(userEmail, callback){
-//     setTimeout(function(){
-//         const Response={
-//             status:true
-//         };callback(Response);
-//     }, 3000)};
+function FetchUser(userid, callback){
+    setTimeout(function(){
+        const FetchUser = {
+            id: userid,
+            name: "Zainab",
+            email: "zainab@gmail.com",
+            age: 21
+        };
+        callback(FetchUser);
+    }, 200);
+}
+console.log("End");
 
+console.log("Before");
 
-//     SendEmail("zainab@gmail.com", callback(response));
-//     console.log("end");
-    
+// Function to Send Email with Callback
+function SendEmail(userEmail, callback){
+    setTimeout(function(){
+        const Response = {
+            status: true
+        };
+        callback(Response);
+    }, 3000);
+}
 
-// console.log("start");
-// const promise= new Promise(function(resolve, reject){
-//     setTimeout(
-//         reject(new Error("i am filled"));
-//         )
-//         resolve(1),2000
-//     );
-// });
+SendEmail("zainab@gmail.com", function(response) {
+    console.log("Email Sent Status:", response.status);
+});
 
-// console.log(promise);
+console.log("End");
 
-// promise.then(function (result) {
-//     console.log(result);
-// })
+// Promise Example
+console.log("Start");
+const promise = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        reject(new Error("I am failed"));
+    }, 2000);
+    resolve(1);
+});
 
-const getAPI=fetch('https://api.github.com/users/ZainabEman');
-getAPI.then((res)=> console.log(res));
-getAPI.catch((err)=>console.log(err.message));
-
-agex
-
-
-
-
-
-
+promise.then(function (result) {
+    console.log(result);
+}).catch(function (error) {
+    console.log("Error:", error.message);
+});s
