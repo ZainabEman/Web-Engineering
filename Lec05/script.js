5689f61d554553c38f74dad3e1f1ee5572d3bd9d
ZainabEman <zainabeman976@gmail.com>
2025-02-28 20:42:15 +0500

    lec 05 update

--- Lec05/script.js
+++ Lec05/script.js
@@ -1,58 +1,54 @@
-// (function fun1(){
-//     console.log("i am invoked");
+// Immediately Invoked Function Expression (IIFE)
+(function fun1(){
+    console.log("I am invoked");
+})();
 
-// })();
-
-
-
-// let greet = function fun1(name){
-//    return `welcome ${name}`;
-// };
- let arr=[1,2,3,3,4,4,1,2];
-
-// function detectDuplicate(arr){
-//     for(i=0;i=arr.length;i++){
-//         //logic to check duplicate
-//     }
-// }
-//arrow function
-//callback functions
-// let name1=((name)=>{
-//     return name;
-// })();
-
-
- const hasDuplicates = arr => arr.filter((item, index, self) => self.indexOf(item) !== index).length > 0;
-
-// console.log(hasDuplicates([1, 2, 3, 4, 5])); // false
-// console.log(hasDuplicates([1, 2, 3, 4, 1])); // true
-
-// const hasDuplicates = arr => arr.some((item, index) => arr.indexOf(item) !== index);
-
-// // Example usage:
-// console.log(hasDuplicates([1, 2, 3, 4, 5])); // false
-// console.log(hasDuplicates([1, 2, 3, 4, 1])); // true
-
-// let add = function sum(a,b){
-//     return a + b;
-// }
-
-// let mul= function product(a,b){return a*b}
-
-// let manipulator=(val1,val2,func)=>{return func(val1,val2)}
-// function counter(){
-//     let count=0;
-//     return function(){
-//          return counter++;
-//     };
-// };
-// let increment =counter();
-// console.log(increment());
-
-function mul(...val){
-    return val.reduce((product,num)=>product*num,1);
+// Function Expression
+let greet = function fun1(name){
+   return `Welcome ${name}`;
 };
 
+// Array Example
+let arr = [1, 2, 3, 3, 4, 4, 1, 2];
+
+// Function to detect duplicates in an array
+const detectDuplicate = arr => arr.filter((item, index, self) => self.indexOf(item) !== index);
+console.log("Duplicates in array:", detectDuplicate(arr));
+
+// Arrow Function Example
+const name1 = name => name;
+console.log("Name Example:", name1("Zainab"));
+
+// Function to check if an array has duplicates
+const hasDuplicates = arr => arr.some((item, index) => arr.indexOf(item) !== index);
+console.log("Has Duplicates (false expected):", hasDuplicates([1, 2, 3, 4, 5]));
+console.log("Has Duplicates (true expected):", hasDuplicates([1, 2, 3, 4, 1]));
+
+// Function Manipulator with Callbacks
+let add = (a, b) => a + b;
+let mul = (a, b) => a * b;
+let manipulator = (val1, val2, func) => func(val1, val2);
+console.log("Sum:", manipulator(3, 5, add));
+console.log("Product:", manipulator(3, 5, mul));
+
+// Counter Closure Example
+function counter(){
+    let count = 0;
+    return function(){
+        return count++;
+    };
+}
+let increment = counter();
+console.log("Increment Counter:", increment());
+console.log("Increment Counter:", increment());
+
+// Function with Rest Parameters
+function multiply(...val){
+    return val.reduce((product, num) => product * num, 1);
+}
+console.log("Multiplication of multiple numbers:", multiply(2, 3, 4, 5));
+
+// Using setTimeout with Function
 setTimeout(() => {
-    console.log(mul(2, 3, 4)); 
+    console.log("Delayed Multiplication:", multiply(2, 3, 4)); 
 }, 2000);
