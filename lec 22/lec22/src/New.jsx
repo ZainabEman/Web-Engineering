22e9b1ff800d8ad1dcbe234636bf015837959bed
ZainabEman <zainabeman976@gmail.com>
2025-03-20 12:17:22 +0500

    updates

--- lec 22/lec22/src/New.jsx
+++ lec 22/lec22/src/New.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+// import React from 'react';
+// import './App.css';
+
+// function New(props) {
+//   console.log(props); // Logs props to the console but does not return anything
+
+//   return (
+//     <div>
+//       <h1>{props.name}!</h1>
+//       <h1>{props.age}!</h1>
+//     </div>
+//   );
+// }
+
+// export default New;
+
+
+import React, { Component } from 'react';
 import './App.css';
 
-function New(props) {
-  console.log(props); // Logs props to the console but does not return anything
+class New extends Component {
+  constructor(props) {
+    super(props); 
+  }
 
-  return (
-    <div>
-      <h1>{props.name}!</h1>
-      <h1>{props.age}!</h1>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>{this.props.name}!</h1>
+        <h1>{this.props.age}!</h1>
+      </div>
+    );
+  }
 }
 
 export default New;
+
