b71210140b1019f2619885e87b1cb04ee7509eed
ZainabEman <zainabeman976@gmail.com>
2025-03-24 09:48:36 +0500

    updates

--- lec 22/lec22/src/FriendList.jsx
+++ lec 22/lec22/src/FriendList.jsx
@@ -1,33 +1,34 @@
-import React, { Component } from 'react';
-import Friend from './Friend.jsx';
+import React, { Component } from "react";
+import Friend from "./Friend.jsx";
 
 export class FriendList extends Component {
     constructor() {
         super();
         this.state = {
             friends: [
-                { name: 'Zainab', email: 'zainab@gmail.com' },
-                { name: 'Sana', email: 'sana@gmail.com' }
+                { name: "Zainab", email: "zainab@gmail.com" },
+                { name: "Sana", email: "sana@gmail.com" }
             ],
-            inputValue: '' // Store input value in state
+            inputValue: "" 
         };
     }
 
     ChangeBookState = () => {
         this.setState({
             friends: [
-                { name: 'Eman', email: 'eman@gmail.com' },
-                { name: 'Noor', email: 'noor@gmail.com' }
+                { name: "Eman", email: "eman@gmail.com" },
+                { name: "Noor", email: "noor@gmail.com" }
             ]
         });
     };
 
     changeInput = (event) => {
         this.setState({
-            // friends: [
-            //     { name: event.target.value, email: this.state.friends[0].email },
-            //     { name: event.target.value, email: this.state.friends[1].email }
-            // ]
+            inputValue: event.target.value, // Store input value
+            friends: [
+                { name: event.target.value, email: this.state.friends[0].email },
+                { name: this.state.friends[1].name, email: this.state.friends[1].email }
+            ]
         });
     };
 
@@ -37,7 +38,13 @@ export class FriendList extends Component {
                 <Friend Name={this.state.friends[0].name} email={this.state.friends[0].email} />
                 <Friend Name={this.state.friends[1].name} email={this.state.friends[1].email} />
                 <button onClick={this.ChangeBookState}>Change state</button>
-                <input type="text" onChange={this.changeInput} placeholder="Enter name..." />
+                <input
+                    type="text"
+                    value={this.state.inputValue}
+                    onChange={this.changeInput}
+                    placeholder="Enter name..."
+                />
+                <p>Updated Name: {this.state.friends[0].name}</p>
             </div>
         );
     }
