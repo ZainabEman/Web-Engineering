b1c386dd7d0af94ff4a5488c1635ad1e456a9c1b
ZainabEman <zainabeman976@gmail.com>
2025-04-11 12:54:50 +0500

    updates

--- Jwt/App.js
+++ Jwt/App.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express');
 const session=require('express-session');
 const bcrypt = require('bcrypt');
@@ -17,9 +19,37 @@ app.use(session({
 function generateToken(user) {
     const token = bcrypt.hashSync(userId.toString(), 10); // Hash the user ID to create a token
     return jwt.sign(
-        id:user.id,
-        email:user.email,
+        {
+            id: user.id,
+            email: user.email
+        },
         process.env.JWT_SECRET_key,
-        expires:'1h' // Token expiration time
+        { expiresIn: '1h' } // Token expiration time
     );
 }
+
+app.post('/login',  (req, res) => {
+    const { email, password } = req.body;
+    const user = users.find(user => user.email === email);
+    if (!user) {
+        return res.status(404).json({ message: 'Invalid email or password' });
+    }
+    const isPasswordValid =  bcrypt.compareSync(password, user.password);
+    if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }//authentication completed now we will not maintain env key instead we will maintain jwt token
+    const token = generateToken(user); // Generate a token for the user
+    return res.status(200).json({ message: 'Login successful', token });
+});
+app.post('/profile', (req, res) => {
+    const authHeader=req.headers.authorization;
+    if(!authHeader) {
+        return res.status(400).json({ message: 'No token provided' });
+    }
+    const token=authHeader.split(' ')[1];
+    try{
+        const decoded=jwt.verify(token,process.env.JWT_SECRET_key);
+        return res.status(200).json({ message: 'Profile data', user: decoded });
+    }catch(err){
+        return res.status(401).json({ message: 'Invalid token' });
+    }
