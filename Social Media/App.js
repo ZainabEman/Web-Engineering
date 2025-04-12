25dfb326fdb85aa061c2045d3b88a499ec8a2b2f
ZainabEman <zainabeman976@gmail.com>
2025-04-12 12:57:42 +0500

    social media blah blah

---
+++ Social Media/App.js
@@ -0,0 +1,32 @@
+const express= require('express');
+const passport= require('passport');
+const session= require('express-session');
+const mongoose= require('mongoose');
+const dotenv = require('dotenv').config();
+const app = express();
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI,require('/Routes/auth.js'))
+
+app.use(session({secret:process.env.SESSION_SECRET}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('view engine','ejs');
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+app.get('/profile', (req,res)=>{
+   if (!req.isAuthenticated()){
+       return res.redirect('/');
+       res.render('profile', {user: req.user});
+
+   }}
+  );
+
+  app.get('/logout', (req,res)=>{
+    req.logout((err)=>{
+        if (err) {return next(err);}
+        res.redirect('/');
+    });
+  });
