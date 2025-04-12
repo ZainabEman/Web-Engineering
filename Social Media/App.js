5a349ef8928271be57f95c43e9e0c38e4b73690a
ZainabEman <zainabeman976@gmail.com>
2025-04-12 13:20:30 +0500

    updates

--- Social Media/App.js
+++ Social Media/App.js
@@ -2,19 +2,16 @@ const express= require('express');
 const passport= require('passport');
 const session= require('express-session');
 const mongoose= require('mongoose');
-const dotenv = require('dotenv').config();
-const app = express();
+const dotenv= require('dotenv').config();
+dot.env.config();
+const app= express();
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI,require('/Routes/auth.js'))
 
-app.use(session({secret:process.env.SESSION_SECRET}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use('view engine','ejs');
-app.get('/',(req,res)=>{
-    res.render('index');
-});
+
+
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log('MongoDB connected'));
 
 app.get('/profile', (req,res)=>{
    if (!req.isAuthenticated()){
